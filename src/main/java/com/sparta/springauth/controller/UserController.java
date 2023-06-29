bb2171f20dd1ccc480c1f46131f6494512ce8123
NayoungKim1212 <kkd04250@naver.com>
2023-06-29 20:01:11 +0900

    Spring Security : JWT 로그인

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -34,14 +34,14 @@ public class UserController {
         return "redirect:/api/user/login-page";
     }
 
-    @PostMapping("user/login")
-    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
-        try {
-            userService.login(requestDto, res);
-        } catch (Exception e) {
-            return "redirect:/api/user/login-page?error";
-        }
-
-        return "redirect:/"; // main으로 redirect
-    }
+//    @PostMapping("user/login")
+//    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+//        try {
+//            userService.login(requestDto, res);
+//        } catch (Exception e) {
+//            return "redirect:/api/user/login-page?error";
+//        }
+//
+//        return "redirect:/"; // main으로 redirect
+//    }
 }
